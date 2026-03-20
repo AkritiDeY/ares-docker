60f5343f6bb4e6d1a37162088c80885879c6e3c0
Akriti Dey <akriti.dey07@gmail.com>
2026-03-21 00:21:29 +0530

    My first microservice project including docker

---
+++ frontend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+const URL = process.env.BACKEND_URL || 'http://localhost:8000/api';
+
+const fetch = (...args) => 
+    import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+app.get('/', async function (req, res) {
+    const options = {
+        method: 'GET',
+    }
+
+    fetch(URL, options)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error('error' + err));
+    
+    try {
+        let response = await fetch(URL, options);
+        response = await response.json();
+        res.render('index', response);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Error fetching data');
+    }
+});
+
+app.listen(3000, () => {
+    console.log(`Ares Frontend is successfully running at http://localhost:3000`);
+});
